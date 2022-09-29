90722053458aa7ecb9e6dda7998909aa44c4e445
Alexander Kotegov <s.kotegov2010@yandex.ru>
2022-09-29 23:39:12 +0500

    added logger

--- db/db_csv.py
+++ db/db_csv.py
@@ -1,1 +1,52 @@
-pass
+import hb_functions as f
+import gui as g
+import handbook_base as hb
+
+
+   
+
+csv_click()
+
+def csv_index_plus(data) -> int:
+    'Find missed index.'
+    for i in range(1, len(data)):
+        old_index = int(data[i].split(";")[0])
+        if old_index != i: break
+    return i
+
+
+def csv_find_to_id(id, data) -> str:
+    "In process. Find element to column by column's name."
+    for i in range(1, len(data)):
+            print(data[i])
+
+def create_csv(data, path_file = 'handbook.csv'):
+    'Create a new empty handbook.'
+    with open(path_file, 'w', encoding="utf-8") as file:
+        file.write(data.replace(' ', ';'))
+
+
+def csv_header(data) -> str:
+    return data[0].split(';')
+
+
+def csv_read(path_file = 'handbook.csv') -> str:
+    with open(path_file, 'r', encoding="utf-8") as csvfile:     
+        data = csvfile.readlines()
+    return data
+
+def csv_add(data, path_file = 'handbook.csv') -> str:
+    'In process. Add record by id.'
+    #id = f.csv_index_plus(data)
+    #print(id)
+    with open(path_file, 'w', encoding="utf-8") as file:
+        file.write(f'{csv_header(data)}')
+        file.write(f'{data}')
+    return data
+
+def csv_del_to_id(id, data) -> str:
+    'In process. Delete record by id.'
+    for i in range(1, len(data)):
+        old_index = int(data[i].split(";")[0])
+        if old_index != i: break
+    return i
--- functions.py
+++ functions.py
@@ -78,4 +78,42 @@ def type_txt():
 # Выбран тип .csv
 def type_csv():
     print('Для работы выбран тип .csv')
+    def csv_click():
+
+        while True:
+            match g.get_operation():
+                case 'new':      # add
+                    # path = g.request('Path and name of the file: ')
+                    path = 'handbook.csv'
+                    data = g.request('Enter names columns with separator "space": ')
+                    # print(data)
+                    hb.create_csv(data, path)
+                    break
+                case 'add':
+                    path = g.request('Path and name of the file: ')
+                    path = 'handbook.csv'
+                    data = hb.csv_read(path)
+                    csv_header = csv_header(data)
+                    hb.csv_add_to_id()
+                    hb.add_new_data(f.new_data(f.new_id(data),
+                                                g.get_lname(),
+                                                g.get_fname(),
+                                                g.get_phone_num()))
+                    print('Запись успешно добавлена!')
+                case 'del':
+                    path = g.request('Path and name of the file: ')
+                    path = 'handbook.csv'
+                    data = hb.csv_read(path)
+                    csv_header = csv_header(data)
+
+
+                    hb.delete_data(g.get_id(), data)
+                    print('Запись успешно удалена!')
+                case 'find':
+                    path = g.request('Path and name of the file: ')
+                    path = 'handbook.csv'
+                    data = hb.csv_read(path)
+                    print(f.find(g.get_lname(), data))
+                case 'exit':
+                    break
 
